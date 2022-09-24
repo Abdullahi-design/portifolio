2d16c224a3124339b55ad354972c55e9b72dbe3d
Abdullahi-design <abdullahismohd3000@gmail.com>
2022-09-24 06:44:04 +0100

    hero section

---
+++ components/Hero.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { Cursor, useTypewriter } from 'react-simple-typewriter'
+import BackgroundCircles from './BackgroundCircles'
+import Abdullahi from '../public/Images/me.jpeg'
+import Image from 'next/image'
+import Link from 'next/link'
+
+type Props = {}
+
+const Hero = (props: Props) => {
+    const [text, count] = useTypewriter({
+        words: [
+            "Hi, my name is Abdulahi sani Mohammed",
+            "Guy-who-loves-to-code.tsx",
+            "<ButLovesToSolveProblemsMore />"
+        ],
+        loop: true,
+        delaySpeed: 2000,
+    });
+  return (
+    <div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden'>
+        <BackgroundCircles />
+        <Image className='relative rounded-full mx-auto object-cover' src={Abdullahi} width={200} height={200} alt="" />
+        <div className='z-20'>
+            <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[15px]'>Software engineer</h2>
+            <h1 className='text-5xl lg:text-6xl font-semibold px-10'>
+                <span className='mr-3'>{text}</span>
+                <Cursor cursorColor="#F7AB0A"/>
+            </h1>
+            <div className='pt-5'>
+                <Link href='#about'>
+                    <button className='heroButton'>About</button>
+                </Link>
+                <Link href="#experience">
+                    <button className='heroButton'>Experience</button>
+                </Link>
+                <Link href="#skills">
+                    <button className='heroButton'>Skills</button>
+                </Link>
+                <Link href="projects">
+                    <button className='heroButton'>Projects</button>
+                </Link>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
